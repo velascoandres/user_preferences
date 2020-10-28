3ac4ee7992049823a5e45d6bdd6cfaa59e31716a
andres velasco <velasco.andrs@gmail.com>
2020-10-28 01:01:42 -0500

    guardar y mostrar ultima pagina ya funciona

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,10 @@ import 'package:user_preferences/src/pages/home_page.dart';
 import 'package:user_preferences/src/pages/settings_page.dart';
 import 'package:user_preferences/src/shared_prefs/preferencias_usuario.dart';
 
-void main() {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   final prefs = new PreferenciasUsuarios();
-  prefs.initPrefs();
+  await prefs.initPrefs();
   runApp(MyApp());
 }
 
@@ -18,8 +18,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Preferencias',
-      // initialRoute: prefs.ultimaPagina,
-      initialRoute: HomePage.routeName,
+      initialRoute: prefs.ultimaPagina,
+      // initialRoute: HomePage.routeName,
       routes: {
         HomePage.routeName: (BuildContext context) => HomePage(),
         SettingsPage.routeName: (BuildContext context) => SettingsPage(),
